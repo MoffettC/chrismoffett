27d9691fcb1a3aeb516adddcc26e60663e29afe3
Christopher Moffett <christophmoffett@gmail.com>
2018-07-12 08:18:25 -0700

    Navigation bar with animate New JS/Jquery/bootstrap cdn links Font has improved responsiveness

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
-	 $('.header').height($(window).height());
-})
+
+$('.nav-link, .navbar-brand, .new-button').click(function() { //jQuery slim version does not support animate!
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top
+    }, 800);
+});
