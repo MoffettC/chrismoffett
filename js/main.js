3e1ef6b387fe200bb5794d5fa4abfbfbf85b8675
Christopher Moffett <christophmoffett@gmail.com>
2018-07-12 19:33:16 -0700

    Stripped out custom css and began replacing w bootstrap

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 $('.nav-link, .navbar-brand, .new-button').click(function() { //jQuery slim version does not support animate!
     var sectionTo = $(this).attr('href');
     $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top
+      scrollTop: $(sectionTo).offset().top -54 //-54px because fixed navbar top overlays on content
     }, 800);
+});
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
 });
