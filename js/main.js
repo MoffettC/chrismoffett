a60a2ceda731e42fb628e51d6ad33f1167504195
Christopher Moffett <christophmoffett@gmail.com>
2018-07-13 07:31:22 -0700

    Skills title Poss scrolling effect later

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $('.nav-link, .navbar-brand, .new-button').click(function() { //jQuery slim version does not support animate!
     var sectionTo = $(this).attr('href');
     $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top -54 //-54px because fixed navbar top overlays on content
+      scrollTop: $(sectionTo).offset().top  //-54px because fixed navbar top overlays on content
     }, 800);
 });
 
@@ -10,10 +10,25 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
-// if(screen.width >= 768) {
-//     $('.navbar').addClass('navbar-fixed-top');
-//     //$('body').css("padding-top", 80);
-// } else {
-//     $('.navbar').removeClass('navbar-fixed-top navbar-fixed').addClass("navbar-static-top");
-//     $('body').css({"padding-top": 0, "margin-top": 0});
-// }
+
+//Scrolling effect....
+// var lastScrollTop = 0;
+// $(window).on('scroll', function () {
+//     var scrollTop = $(window).scrollTop();
+// 	console.log(scrollTop);
+//     if (scrollTop < 70 && scrollTop > lastScrollTop) { //scroll down
+//     	var sectionTo = $(this).attr('about');
+//         $('html, body').stop().animate({
+// 		      scrollTop: $(sectionTo).offset().top  //-54px because fixed navbar top overlays on content
+// 		    }, 500);
+        
+//     }
+//     else if (scrollTop < 780 && scrollTop < lastScrollTop) { //scroll up
+//     	var sectionTo = $(this).attr('navtop');
+//         $('html, body').stop().animate({
+// 		    scrollTop: $(sectionTo).offset().top  //-54px because fixed navbar top overlays on content
+// 		}, 100); 
+//     }
+
+//   lastScrollTop = scrollTop;
+// });
