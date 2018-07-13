35b43b78f0f9628a7b410e6d2badbe104243fff9
Christopher Moffett <christophmoffett@gmail.com>
2018-07-13 08:55:11 -0700

    Readded pixel offset for sticky nav, along with responsive padding

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $('.nav-link, .navbar-brand, .new-button').click(function() { //jQuery slim version does not support animate!
     var sectionTo = $(this).attr('href');
     $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top  //-54px because fixed navbar top overlays on content
+      scrollTop: $(sectionTo).offset().top -54 //-54px because fixed navbar top overlays on content
     }, 800);
 });
 
