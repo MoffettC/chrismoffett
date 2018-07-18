9cf4181c48decae9f85b900f4a2f17ca62e99d0e
Christopher Moffett <christophmoffett@gmail.com>
2018-07-17 19:23:19 -0700

    Refactored JS to change nav bar color depending on scroll Changed columns in skills to be responsive to size

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
 
-$('.nav-link, .navbar-brand, .new-button').click(function() { //jQuery slim version does not support animate!
-	var scrollY = 54;
+$(document).ready(function() {
+	$('.nav-link, .navbar-brand, .new-button').click(function() { //jQuery slim version does not support animate!
+		var scrollY = 54;
 
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	 	scrollY = 180;
-	}
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		 	scrollY = 180;
+		}
 
-    var sectionTo = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top - scrollY //-54px because fixed navbar top overlays on content
-    }, 800);
+	    var sectionTo = $(this).attr('href');
+	    $('html, body').animate({
+	      scrollTop: $(sectionTo).offset().top - scrollY //-54px because fixed navbar top overlays on content
+	    }, 800);
+	});
+
+	$('.navbar-nav>li>a').on('click', function(){
+	    $('.navbar-collapse').collapse('hide');
+	});
+
+	$('.mid .primary ul').filter(function(){
+	    return $(".primary li").length > 5
+	}).addClass('twoColumns');
+
+	$('.mid .secondary ul').filter(function(){
+	    return $(".secondary li").length > 5
+	}).addClass('twoColumns');
+
+	$('.mid .tertiary ul').filter(function(){
+	    return $(".tertiary li").length > 5
+	}).addClass('twoColumns');
+
+	$(document).ready(function(){       
+	   var scroll_start = 0;
+	   var startchange = $('#about');
+	   var offset = startchange.offset();
+
+	   if (startchange.length){
+		   $(document).scroll(function() { 
+		      scroll_start = $(this).scrollTop();
+		      if(scroll_start > offset.top - 100) {
+		          $(".navbar-inverse").css('background-color', '#4b9eec');
+		       } else {
+		          $('.navbar-inverse').css('background-color', '#000000');
+		       }
+		   });
+	    }
+	});
+
 });
-
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
-
-
-
-
 //Scrolling effect....
 // var lastScrollTop = 0;
 // $(window).on('scroll', function () {
